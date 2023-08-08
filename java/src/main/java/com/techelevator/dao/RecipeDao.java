01638757f8b882abb5145f10fb4fe42ae15f3ef0
Sadik Mohamed <sadikmohamed205@gmail.com>
2023-08-08 13:44:58 -0400

    Create Functionality To Remove Recipe From Favorites

--- java/src/main/java/com/techelevator/dao/RecipeDao.java
+++ java/src/main/java/com/techelevator/dao/RecipeDao.java
@@ -9,7 +9,7 @@ public interface RecipeDao {
 
     public List<RecipeDto> getAllFavoritedRecipes(int userId);
     public List<RecipeDto> getDashboardRecipes(int userId);
-    public  RecipeDto addFavoriteRecipe(RecipeDto recipeDto, int userId);
-
+    public RecipeDto addFavoriteRecipe(RecipeDto recipeDto, int userId);
+    public void removeRecipeFromFavorites(RecipeDto recipeDto, int userId);
 
 }
