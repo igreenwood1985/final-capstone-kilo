ff69f8beaf51daab61a7436d4288ef841a179035
Colin Ramsby <ramsbycolin@gmail.com>
2023-08-08 12:46:25 -0400

    progress

--- vue/src/services/RecipeService.js
+++ vue/src/services/RecipeService.js
@@ -29,11 +29,11 @@ export default {
         console.log("hi");
       })
       console.log(appends);
-      return http.get('?type=public&beta=false&q=' + searchTerm + '&app_id=' + apiID + '&app_key=' + apiKey + '&random=true' + appends)
+      return http.get('?type=public&beta=false&q=' + searchTerm + '&app_id=' + apiID + '&app_key=' + apiKey + '&random=true' + appends);
     },
 
-    getRecipeByUri() {
-      return http.get('by-uri?type=public&beta=false&uri=http://www.edamam.com/ontologies/edamam.owl#recipe_e4108dbf7f340c9b08592e57e669353f&app_id=be14acd0&app_key=b9156f49c2db2a7389974ba9261c3628&field=uri&field=label&field=image&field=images&field=source&field=url&field=shareAs&field=yield&field=dietLabels&field=healthLabels&field=cautions&field=ingredientLines&field=ingredients&field=calories&field=glycemicIndex&field=totalCO2Emissions&field=co2EmissionsClass&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=totalNutrients&field=totalDaily&field=digest&field=tags&field=externalId')
+    getRecipeByUri(uri) {
+      return http.get('by-uri?type=public&beta=false&uri=' + uri + '&app_id=be14acd0&app_key=b9156f49c2db2a7389974ba9261c3628&field=uri&field=label&field=image&field=images&field=source&field=url&field=shareAs&field=yield&field=dietLabels&field=healthLabels&field=cautions&field=ingredientLines&field=ingredients&field=calories&field=glycemicIndex&field=totalCO2Emissions&field=co2EmissionsClass&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=totalNutrients&field=totalDaily&field=digest&field=tags&field=externalId');
     }
 
 }
