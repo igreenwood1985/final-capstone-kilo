3d093945ae40aa7899cf86317ef17ce01ce9d623
Troy Smits <troyjsmits@gmail.com>
2023-08-09 12:39:28 -0400

    Format dashboard, style cards

--- vue/src/components/FavoritedCard.vue
+++ vue/src/components/FavoritedCard.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="card">
-    <!-- <router-link v-bind:to="{name: 'recipe-details', params: {id : recipe.uri}}"> -->
-     <!-- <h2 class="recipe-name">{{ recipe.label }}</h2> -->
-    <!-- </router-link> -->
+  <div class="main">
     <img v-bind:src="recipe.img" id="image" />
+    <router-link v-bind:to="{name: 'recipe-details', params: {id : recipe.uri}}">
+    <h2 class="recipe-name">{{ recipe.label }}</h2>
+    </router-link>
     <p>Cuisine Type: {{recipe.cuisineType}}</p>
     <button v-on:click="removeFromFavorites()">Unfavorite</button>
     <p>Calories: {{ recipe.calories }}</p>
@@ -11,6 +11,7 @@
     <p>{{ recipe.totalTime }} minutes</p>
 
   </div>
+  
 </template>
 
 <script>
@@ -43,32 +44,28 @@ export default {
 </script>
 
 <style>
-.card {
+.main {
   border: 2px solid black;
   border-radius: 10px;
   display: inline-block;
   width: 275px;
-  height: 300px;
+  height: 400px;
   margin: 20px;
   background-color: white;
+  overflow: hidden;
 }
 
-
-#image {
-  width: 50%;
-  height: auto;
-  aspect-ratio: 303 / 303;
-}
-
-
-
-
-.card .recipe-name {
+.main .recipe-name {
   font-size: 1.2rem;
   text-align: center;
 }
 
-.card .time-to-make {
+.main .time-to-make {
   font-size: 1rem;
+}
+
+#image {
+   width: 100%; 
+   height: 50%;
 }
 </style>
