618e09102cdeace8fe6e19dc0cb38bf0abda3412
Troy Smits <troyjsmits@gmail.com>
2023-08-10 13:30:03 -0400

    Create MyMeals component to display all meals on one page

---
+++ vue/src/views/MyMealsView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <my-meals />
+  </div>
+</template>
+
+<script>
+import MyMeals from '../components/MyMeals.vue'
+
+export default {
+    name: 'my-meals-view',
+    components: {
+        MyMeals
+    }
+
+}
+</script>
+
+<style>
+
+</style>
