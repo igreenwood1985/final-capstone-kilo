4456b737a5db302aa8d629329d4911a35555e811
Sadik Mohamed <sadikmohamed205@gmail.com>
2023-08-10 10:17:40 -0400

    pull

---
+++ vue/src/components/MealCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import AccountService from '../services/AccountService'
+
+export default {
+    name: "meal-card",
+      props: {
+    recipe: Object,
+    enableAdd: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      healthTags: "",
+      favorited: false,
+    };
+  },
+  methods: {
+
+
+        addToMealPlan() {
+      AccountService.addMealToPlan(this.formatMeal()).then((response) => {
+        return response.status == 200 //change to 201 when server status codes updated
+      });
+      this.favorited = true;
+    },
+    formatMeal() {
+      const formattedMeal = {
+       //To do based on database columns 
+      };
+      console.log(formattedMeal);
+      return formattedMeal;
+    }
+  },
+
+
+
+  }
+
+
+</script>
+
+<style>
+
+</style>
