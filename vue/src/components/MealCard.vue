c8e36626ebdb313be988d36cb608efd2c05e1cc1
Troy Smits <troyjsmits@gmail.com>
2023-08-14 12:21:34 -0400

    MealCard styling

--- vue/src/components/MealCard.vue
+++ vue/src/components/MealCard.vue
@@ -1,28 +1,34 @@
 <template>
-  <div class="main">
-    <!-- <router-link v-bind:to="{ name: 'meal-editor', params: {id: meal.meal_id} }">
-      <h2 class="meal-name" v-on:click="updateCurrentMealInStore()">{{ meal.name }}</h2>
-    </router-link>
-    <h2 class="meal-description">{{ meal.description }}</h2> -->
-    <mini-fav-card
-      v-for="recipe in updateArray"
-      v-bind:recipe="recipe"
-      v-bind:key="recipe.id"
-      v-bind:enable-add="true"
-      class="favorited-card"
-      v-show="updateArray.length !== 0"
-    />
+  <div class="meal-card">
+    <div class="meal-card-heading">
+      <router-link
+        v-bind:to="{ name: 'meal-editor', params: { id: meal.meal_id } }"
+      >
+        <h2 class="meal-name" v-on:click="updateCurrentMealInStore()">
+          {{ meal.name }}
+        </h2>
+      </router-link>
+      <h2 class="meal-description">{{ meal.description }}</h2>
+    </div>
+    
+    <div class="images-box">
+<div class="images-loop" v-for="recipe in meal.recipes" v-bind:key="recipe.id">
+      <div class="inner-images">
+        <img v-bind:src="recipe.img" alt="Image of Recipe" />
+      </div>
+      
+    </div>
+    </div>
+    
+
   </div>
 </template>
 
 <script>
-import MiniFavCard from './MiniFavCard.vue';
-
-
 export default {
-  components: { MiniFavCard },
-    name: "meal-card",
-      props: {
+  components: {},
+  name: "meal-card",
+  props: {
     meal: Object,
     enableAdd: {
       type: Boolean,
@@ -31,58 +37,67 @@ export default {
   },
   computed: {
     updateArray() {
-     return this.$store.state.favoritedRecipes.slice(0, 3);
-    }
+      return this.$store.state.meals.slice(0, 3);
+    },
   },
   data() {
-    return {
-  
-    };
+    return {};
   },
   methods: {
     updateCurrentMealInStore() {
       this.$store.commit("SET_CURRENT_MEAL", this.meal.meal_id);
-
-    }
-
-
-
+    },
   },
-
-
-
-  }
-
-
+};
 </script>
 
 <style scoped>
-.main {
+.meal-card {
   border: 2px solid black;
   border-radius: 10px;
   display: inline-block;
-  max-width: 12rem;
-  max-height: 12rem;
+  width: 12rem;
+  height: 12rem;
   margin: 20px;
   background-color: white;
   overflow: hidden;
 }
 
+.meal-card-heading {
+  border: solid 2px black;
+  background-color: #f8f9fa;
+  padding: 2px;
+}
 
-
-
-.main .recipe-name {
-
-  font-size: 1.2rem;
+.meal-name {
+  font-size: 1rem;
   text-align: center;
 }
 
-.main .time-to-make {
-  font-size: 1rem;
+.meal-description {
+  font-size: 0.75rem;
+  text-align: center;
 }
 
-#image {
-   width: 100%; 
-   height: 50%;
+img {
+  display: inline-block;
+  max-height: 3rem;
+  max-width: 2rem;
+  border-radius: 2px;
+}
+
+.images-box {
+  display: flex;
+  justify-content: center;
+  margin-top: .5rem;
+  flex-wrap: wrap;
+}
+
+
+.inner-images {
+  display: inline-block;
+  border: 4px solid blue;
+  margin: 3px;
+
 }
 </style>
