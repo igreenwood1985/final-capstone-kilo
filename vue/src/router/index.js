3828ff3616ff22273796d47876440ed0ea7bee3e
Colin Ramsby <ramsbycolin@gmail.com>
2023-08-17 10:50:04 -0400

    removed login requirement to see recipe details

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -57,7 +57,7 @@ const router = new Router({
       name: 'recipe-details',
       component: RecipeDetailsView,
       meta: {
-        requiresAuth: true
+        requiresAuth: false
       }
     },
     {
